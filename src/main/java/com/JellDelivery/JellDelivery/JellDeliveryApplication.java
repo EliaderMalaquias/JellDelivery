593c6699aa44517fb884df2f83a98275f08b5fe4
Eliader Malaquias <joseeliader190@gmail.com>
2025-07-01 21:21:29 -0300

    Configuracao iniciais

---
+++ src/main/java/com/JellDelivery/JellDelivery/JellDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.JellDelivery.JellDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JellDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JellDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JellDelivery/JellDelivery/JellDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JellDelivery.JellDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JellDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
