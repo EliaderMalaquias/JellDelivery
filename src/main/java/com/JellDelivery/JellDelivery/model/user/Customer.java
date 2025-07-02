86118ddbbeabfb0a9c699bc32c58ae5430e18c2c
Eliader Malaquias <joseeliader190@gmail.com>
2025-07-01 21:36:04 -0300

    Criando as entidades

---
+++ src/main/java/com/JellDelivery/JellDelivery/model/user/Customer.java
@@ -0,0 +1,27 @@
+package com.JellDelivery.JellDelivery.model.user;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "customerUser_db")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String phone;
+
+}
